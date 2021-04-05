366124a3ed4bd0fbdabcdd86ee471e70178835c0
Pedro <pedro.henrique@usercentrics.com>
2021-04-05 09:24:43 +0100

    1.10.4

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "UsercentricsUI",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "UsercentricsUI",
+            targets: ["UsercentricsUI"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://bitbucket.org/usercentricscode/usercentrics-smp-sdk", from: "1.10.4")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UsercentricsUI",
+            url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui/downloads/UsercentricsUI.xcframework.zip"
+        )
+    ]
+)
