ce3b7b42497d4e08f0d79c2bc02a352e198270c8
Usercentrics <mobile@usercentrics.com>
2021-06-15 16:14:24 +0000

    v1.12.2

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://bitbucket.org/usercentricscode/usercentrics-spm-sdk", from: "1.12.1")
+        .package(url: "https://bitbucket.org/usercentricscode/usercentrics-spm-sdk", from: "1.12.2")
     ],
     targets: [
         .binaryTarget(
             name: "UsercentricsUI",
-            url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui/downloads/UsercentricsUI-1.12.1.xcframework.zip",
-            checksum: "3804d83aebebccc753b71cd20d9421b0e17df9479034e430f6ebdcdcfc445271"
+            url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui/downloads/UsercentricsUI-1.12.2.xcframework.zip",
+            checksum: "b15374856897ae75618e164ded9a2f024da222fdd44d3f077df6d88eac06e9ba"
         )
     ]
 )
