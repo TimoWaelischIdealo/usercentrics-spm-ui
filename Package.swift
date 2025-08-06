077efdc454633d95c252e9262c0fcd952bdcd932
developer <developer@usercentrics.com>
2025-08-06 19:17:38 +0000

    v2.22.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .binaryTarget(
             name: "UsercentricsUIFramework",
             url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui/downloads/UsercentricsUI-2.22.0.xcframework.zip",
-            checksum: "251e0ef62190479e4a9b7997fcbface3e63d340e78930bf197f7b70a5a95e15a"
+            checksum: "e7ed26eda89bf0681a721ba6d3744a8c5492485cd369c8b3402434c1abd1c5f6"
         ),
         .target(
             name: "UsercentricsUI",
